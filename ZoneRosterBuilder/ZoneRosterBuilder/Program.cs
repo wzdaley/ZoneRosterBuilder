a21b981471d247f2f64438b5ee76fe43299f5831
wzdaley <41518716+wzdaley@users.noreply.github.com>
2025-09-28 01:08:00 -0500

    Update Program.cs

--- ZoneRosterBuilder/ZoneRosterBuilder/Program.cs
+++ ZoneRosterBuilder/ZoneRosterBuilder/Program.cs
@@ -1,8 +1,12 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using ZoneRosterBuilder;
+using Blazored.LocalStorage;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
-builder.Services.AddScoped(sp => new HttpClient());
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();
